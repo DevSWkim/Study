ea62e10d181675950857582d12b8644adfc6d49e
WOONG <kimsewoong@outlook.com>
2019-10-18 23:28:38 +0800

    add method

--- src/main/java/com/mark/book/RestApiController.java
+++ src/main/java/com/mark/book/RestApiController.java
@@ -11,49 +11,70 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RestApiController {
 
-	private Map<String, String> userData = new HashMap<String, String>();
+	private Map<String, Data> userData = new HashMap<String, Data>();
 	
 	@RequestMapping("/")
 	public String getMain() {
-		return "Hello";
+		return "총 회원 수 : " + userData.size();
 	}
 	
-	@RequestMapping(value="/users", method=RequestMethod.GET)
-	public String getId(@RequestParam(value="id") String id) {
+	@RequestMapping(value="/users", method= {RequestMethod.GET, RequestMethod.POST})
+	public String getAllUser() {
 		
-		System.out.println("Entered ID : " + id);
+		return userData.toString();
+	}
+	
+	@RequestMapping(value="/user", method= {RequestMethod.GET, RequestMethod.POST})
+	public String getUser(@RequestParam(value="id") String id) {
 		
-		if(!userData.isEmpty()) {
-			if(!userData.containsKey(id)) {
-				
-				System.out.println(userData.size());
-				
-				return "We have the Id information.";
-			}
+		if(!userData.containsKey(id)) {
+			return id + "님은 회원이 아닙니다.";
 		} else {
-			return "We have no user Data.";
+			return userData.get(id).toString();
+		}
+	}
+	
+	@RequestMapping(value="/users/add", method= {RequestMethod.GET, RequestMethod.POST})
+	public Map<String, Data> addUser(@RequestParam(value="id") String id,
+									 @RequestParam(value="name") String name,
+									 @RequestParam(value="password") String password) {
+
+		Data data = new Data(id, password, name);
+		
+		userData.put(id, data);
+		
+		return userData;
+	}
+	
+	@RequestMapping(value="/users/update", method= {RequestMethod.GET, RequestMethod.POST})
+	public String updateUser(@RequestParam(value="id") String id,
+									 @RequestParam(value="name") String name,
+									 @RequestParam(value="password") String password) {
+
+		if(!userData.containsKey(id)) {
+			return id + "님은 회원이 아닙니다.";
+		} else {
+			if(userData.get(id).getPassword().equals(password)) {
+				String beforeName = userData.get(id).getName();
+				userData.get(id).setName(name);
+				return id + "님의 이름을 " + beforeName + "에서 " + name + "으로 변경하였습니다.";
+			} else {
+				return id + "님의 패스워드가 올바르지 않습니다.";
+			}
 		}
 		
-		return userData.get(id);
 	}
 	
-	@RequestMapping(value="/users", method=RequestMethod.POST)
-	public String setId(@RequestParam(value="id") String id) {
+	@RequestMapping(value="/users/delete", method=RequestMethod.GET)
+	public String deleteUser(@RequestParam(value="id") String id) {
+
+		if(!userData.containsKey(id)) {
+			return id + "님은 회원이 아닙니다.";
+		} else {
+			userData.remove(id);
+			return id + "님이 정상적으로 삭제되었습니다.";
+		}
 		
-		System.out.println("added ID : " + id);
-		
-		return userData.put(id, "TEST");
 	}
-	
-	
-//	@RequestMapping(value="/users", method=RequestMethod.POST)
-//	public int increaseWins(){   
-//		{ return ++Score.WINS;
-//	}
-//
-//	@RequestMapping(value="/users", method=RequestMethod.GET)
-//	public Data getData() {
-//		return Score.WINS;   
-//	}
 
 }
